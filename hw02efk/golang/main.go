53790c071126188b49405d155db17080217990de
Стрельников Дмитрий Валерьевич <dmvstrelnikov@beeline.ru>
2022-03-18 10:35:37 +0300

    upd

--- hw02efk/golang/main.go
+++ hw02efk/golang/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"os"
 	"strconv"
 
 	"syreclabs.com/go/faker"
@@ -36,13 +37,7 @@ func main() {
 func triggerHandler(w http.ResponseWriter, r *http.Request) {
 
 	HTTPMethodList := []int64{404, 500, 200, 504, 502, 400}
-
-	//	file, err := os.OpenFile("/go/src/app/application.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 644)
-	//	if err != nil {
-	//		panic(err.Error())
-	//	}
-	//
-	//	log.SetOutput(file)
+	log.SetOutput(os.Stdout)
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetLevel(log.InfoLevel)
 
@@ -56,7 +51,15 @@ func triggerHandler(w http.ResponseWriter, r *http.Request) {
 		newLog.Body.Department = faker.Commerce().Department()
 		newLog.Body.Company = faker.Company().Name()
 
-		log.WithField("data", newLog).Info()
+		//log.WithField("data", newLog).Info()
+		//fmt.Fprint(w, newLog)
+		log.WithFields(log.Fields{
+			"UUID":       uuid.New(),
+			"Status":     HTTPMethodList[rand.Intn(cap(HTTPMethodList)-1)],
+			"City":       faker.Address().City(),
+			"Department": faker.Commerce().Department(),
+			"Company":    faker.Company().Name(),
+		}).Info("new log")
 	}
 
 	returnText := "Generate " + strconv.Itoa(loop) + " logs"
